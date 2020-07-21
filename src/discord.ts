ad403fb26f71ee6d73d0bfae2210398580543dca
Carter Grimmeisen <cpgrimmeisen@gmail.com>
2020-07-20 20:41:33 -0500

    Updates and removed .yarn from source control

--- src/discord.ts
+++ src/discord.ts
@@ -1,81 +1,86 @@
-import { Discord, Command, CommandMessage } from '@typeit/discord'
-import parseDuration from 'parse-duration'
+import { Discord, Command, CommandMessage } from '@typeit/discord';
+import parseDuration from 'parse-duration';
 
-const FUNNY_NUMBER = 420_69_000
+const FUNNY_NUMBER = 420_69_000;
 
 @Discord()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export abstract class CornerDiscord {
   @Command('corner :time')
   private async corner(message: CommandMessage) {
     if (
-      message.member?.roles.cache.find(each => each.name === 'Corner Abuser')
+      message.member?.roles.cache.find((each) => each.name === 'Corner Abuser')
     ) {
       return message.reply(
         'you have abused the corner and cannot corner others!',
-      )
+      );
     }
 
     if (message.member?.voice.channel?.name === 'The Corner') {
-      return message.reply("you can't bring others down with you!")
+      return message.reply('you can\'t bring others down with you!');
     }
 
-    const { time } = message.args as Record<string, string>
+    const { time } = message.args as Record<string, string>;
     const users = message.mentions.users.map(
-      each =>
+      (each) =>
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         message.guild!.members.cache.find(
-          cached =>
+          (cached) =>
             cached.user.username === each.username &&
             cached.user.tag === each.tag,
         )!,
-    )
+    );
 
-    if (!users.length) {
-      return message.reply('you must specify users to put in the corner!')
+    if (users.some((user) => user.voice.channelID === undefined)) {
+      return message.reply(
+        'you cannot corner users that are not in a voice channel!',
+      );
     }
 
-    let duration: number
+    if (!users.length) {
+      return message.reply('you must specify users to put in the corner!');
+    }
+
+    let duration: number;
     try {
-      duration = parseDuration(time) || 0
+      duration = parseDuration(time) || 0;
     } catch {
       return message.reply(
-        'you must specify and appropriate duration! (ie. 30s or 1m)',
-      )
+        'you must specify and appropriate duration! (ie. 30s or 1m)!',
+      );
     }
 
     if (duration != FUNNY_NUMBER) {
       if (duration < 1_000) {
         return message.reply(
           'you cannot put people into the corner for less than a second!',
-        )
+        );
       }
 
       if (duration > 60_000) {
         return message.reply(
           'you cannot put people into the corner for longer than a minute!',
-        )
+        );
       }
     }
 
     const theCorner = message.guild?.channels.cache.find(
-      each => each.name === 'The Corner',
-    )
+      (each) => each.name === 'The Corner',
+    );
 
     if (!theCorner) {
       return message.reply(
-        "your server must have a channel called 'The Corner' to put users into!",
-      )
+        'your server must have a channel called "The Corner" to put users into!',
+      );
     }
 
     const inTheCorner = message.guild?.roles.cache.find(
-      each => each.name === 'In The Corner',
-    )
+      (each) => each.name === 'In The Corner',
+    );
 
     if (!inTheCorner) {
       return message.reply(
-        "your server must have a role called 'In The Corner' that holds users captive!",
-      )
+        'your server must have a role called "In The Corner" that holds users captive!',
+      );
     }
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -85,39 +90,40 @@ export abstract class CornerDiscord {
       type: 'conjunction',
     }).format(
       users.map(
-        each => each.nickname || each.displayName || each.user.username,
+        (each) => `<@${each.user.id}>`,
       ),
-    )
+    );
 
     message.channel.send(
       `${usernames} ${users.length > 1 ? 'are' : 'is'} being punished!`,
-    )
+    );
 
     return Promise.all(
-      users.map(async each => {
-        if (each.roles.cache.find(each => each.name === 'In The Corner')) return
+      users.map(async (each) => {
+        if (each.roles.cache.find((each) => each.name === 'In The Corner'))
+          return;
 
-        const previous = each.voice.channel
+        const previous = each.voice.channel;
 
-        await each.roles.add(inTheCorner)
-        if (each.voice.channelID) await each.voice.setChannel(theCorner)
+        await each.roles.add(inTheCorner);
+        if (each.voice.channelID) await each.voice.setChannel(theCorner);
 
-        return new Promise(res => {
+        return new Promise((res) => {
           setTimeout(
             async () => {
               if (duration === FUNNY_NUMBER)
-                message.channel.send("Just kidding, that's bullying")
+                message.channel.send('Just kidding, that\'s bullying');
 
-              await each.roles.remove(inTheCorner)
+              await each.roles.remove(inTheCorner);
 
-              if (each.voice.channelID) await each.voice.setChannel(previous)
+              if (each.voice.channelID) await each.voice.setChannel(previous);
 
-              res()
+              res();
             },
             duration === FUNNY_NUMBER ? 15_000 : duration,
-          )
-        })
+          );
+        });
       }),
-    )
+    );
   }
 }
